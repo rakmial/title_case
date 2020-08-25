82155bf9b3babc5f33fd97cdad4b711a8d5cccf3
Joshua Lovelace <rakmial@gmail.com>
2020-08-25 12:07:37 -0700

    test 2 fails successfully

--- spec/title_case_spec.rb
+++ spec/title_case_spec.rb
@@ -5,4 +5,8 @@ describe('#title_case') do
   it("capitalizes the first letter of a word") do
     expect(title_case("ludwig")).to(eq("Ludwig"))
   end
+  it("capitalizes the first letter of all words in a multiword title") do
+    expect(title_case("godel escher bach")).to(eq("Godel Escher Bach"))
+  end
+  
 end
